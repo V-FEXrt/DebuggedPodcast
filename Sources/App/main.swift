4a91a4f41b8ce3e25f7b52490b22fd6ec5ca15e9
Ashley Coleman <ashley78@ksu.edu>
2017-04-06 21:55:54 -0500

    Linux/Mac path fix

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -7,7 +7,7 @@ try drop.addProvider(VaporSQLite.Provider.self)
 drop.preparations.append(User.self)
 
 drop.get { req in
-    return try drop.view.make("../../Public/html/index.html")
+    return try drop.view.make(drop.config["app", "root_path"]?.string ?? "")
 }
 
 drop.resource("users", UserController())
